f59434a897085a4c604bb1ab4900b42afe9bfb17
Jesse Morgan <jesse.morgan@willowtreeapps.com>
2020-06-07 18:15:59 -0400

    Initial Commit

---
+++ TempTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TempTracker
+//
+//  Created by Jesse Morgan on 6/7/20.
+//  Copyright © 2020 Jesse Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
